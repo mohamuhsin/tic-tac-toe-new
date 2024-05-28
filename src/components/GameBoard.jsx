d297512293db6f92cb98d80077a8d296b6965729
mohamuhsin <muhsinohoho@outlook.com>
2024-05-28 23:47:43 +0300

    we determined the winner by computing the winner logic and transfering the gameBoard into the App.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,4 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
+export default function GameBoard({ onSelectSquare, board }) {
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     // function handleSelectedSquare(rowIndex, colIndex) {
@@ -29,7 +15,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
